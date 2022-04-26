632518a1cea445db1f94908aa03218b699b230f5
Bella <dark.shadow2019@yahoo.com>
2022-04-27 00:19:33 +0300

    api call optimizatio

--- core/tools/lastfm.py
+++ core/tools/lastfm.py
@@ -35,7 +35,7 @@ headers = {
 
 def get_artists(username):
     artists = []
-    r = requests.get(API_ROOT, params={'api_key':env('API_KEY'), 'user':username, 'format':'json', 'method':'library.getartists', 'limit':'100'})
+    r = requests.get(API_ROOT, params={'api_key':env('API_KEY'), 'user':username, 'format':'json', 'method':'library.getartists', 'limit':'52'})
     content = r.json()
     for i in range(100):
         try:
